1cf3db54a8ad878c361e59d07f088573f3c75097
Vasil Joskov <v.joskov@xs-software.com>
2017-12-18 20:26:19 +0100

    Day 18.

--- src/com/joskov/advent2017/Day18.java
+++ src/com/joskov/advent2017/Day18.java
@@ -10,26 +10,29 @@ public class Day18 extends Helpers {
     public static void main(String[] args) {
         List<Command> input = readInput();
         partOne(input);
-//        partTwo(input);
+        partTwo(input);
     }
 
     private static void partOne(List<Command> input) {
         Application app = new Application(input);
         while (app.isIndexValid()) {
-//            app.display();
             app.execute();
         }
+        System.out.println("Sound was: " + app.getSound());
     }
 
     private static void partTwo(List<Command> input) {
         Threaded first = new Threaded(input, 0);
-        Threaded second = new Threaded(input, 0);
+        Threaded second = new Threaded(input, 1);
         while (first.isIndexValid() || second.isIndexValid()) {
-            first.display();
             first.execute(second);
-            second.display();
             second.execute(first);
+            if (first.isBlocked() && second.isBlocked()) {
+                break;
+            }
         }
+        System.out.println("Program 0 sent: " + first.getSentValues());
+        System.out.println("Program 1 sent: " + second.getSentValues());
     }
 
     public static List<Command> readInput() {
@@ -45,15 +48,13 @@ public class Day18 extends Helpers {
     public static abstract class BaseApp {
         protected Map<String, Long> registers;
         protected List<Command> commands;
-        protected long index;
+        protected int index;
 
         public BaseApp(List<Command> commands) {
             this.commands = commands;
             registers = new HashMap<>();
             index = 0;
         }
-
-
 
         public boolean isIndexValid() {
             return (index >= 0) && (index < commands.size());
@@ -79,12 +80,22 @@ public class Day18 extends Helpers {
             registers.entrySet().forEach(a -> System.out.printf("%s:%d ", a.getKey(), a.getValue()));
             System.out.println();
         }
+
+        public Command getCurrentCommand() {
+            return commands.get(index);
+        }
     }
 
     public static class Threaded extends BaseApp {
+        protected List<Long> queue;
+        protected int sentValues = 0;
+        protected long id;
+
         public Threaded(List<Command> commands, long id) {
             super(commands);
+            this.id = id;
             set("p", id);
+            queue = new ArrayList<>();
         }
 
         public void execute(Threaded other) {
@@ -92,16 +103,15 @@ public class Day18 extends Helpers {
                 return;
             }
 
-//            System.out.println("Index is: " + index);
-            Command command = commands.get((int) index);
+            Command command = commands.get(index);
             apply(command, other);
         }
 
-        public void apply(Command command, BaseApp other) {
-//            System.out.printf("Execute %s with %s=%s, %s%n", command.type, command.a, get(command.a), get(command.b));
+        public void apply(Command command, Threaded other) {
             switch (command.type) {
                 case SND:
-                    System.out.println("Sending value: " + get(command.a));
+                    sentValues++;
+                    other.addToQueue(get(command.a));
                     moveIndex(1);
                     break;
                 case SET:
@@ -121,7 +131,11 @@ public class Day18 extends Helpers {
                     moveIndex(1);
                     break;
                 case RCV:
-                    System.out.println("Receive value: " + command.a);
+                    if (queue.size() > 0) {
+                        set(command.a, queue.remove(0));
+                        moveIndex(1);
+                    }
+                    break;
                 case JUMP:
                     if (get(command.a) > 0) {
                         moveIndex(get(command.b));
@@ -130,6 +144,10 @@ public class Day18 extends Helpers {
                     }
                     break;
             }
+        }
+
+        private void addToQueue(long value) {
+            queue.add(value);
         }
 
         public long get(String register) {
@@ -144,8 +162,16 @@ public class Day18 extends Helpers {
             registers.put(register, value);
         }
 
+        public boolean isBlocked() {
+            return queue.isEmpty() && getCurrentCommand().getType() == Command.Type.RCV;
+        }
+
         public void moveIndex(long delta) {
             index += delta;
+        }
+
+        public int getSentValues() {
+            return sentValues;
         }
     }
 
@@ -157,17 +183,14 @@ public class Day18 extends Helpers {
         }
 
         public void execute() {
-//            System.out.println("Index is: " + index);
-            Command command = commands.get((int) index);
+            Command command = commands.get(index);
             apply(command);
         }
 
         public void apply(Command command) {
-//            System.out.printf("Execute %s with %s=%s, %s%n", command.type, command.a, get(command.a), get(command.b));
             switch (command.type) {
                 case SND:
                     setSound(get(command.a));
-//                    System.out.println("Setting sound as: " + getSound());
                     moveIndex(1);
                     break;
                 case SET:
@@ -188,8 +211,7 @@ public class Day18 extends Helpers {
                     break;
                 case RCV:
                     if (get(command.a) != 0) {
-                        System.out.println("Sound was: " + getSound());
-                        moveIndex(10000);
+                        index = -1;
                     } else {
                         moveIndex(1);
                     }
@@ -255,6 +277,10 @@ public class Day18 extends Helpers {
             String a = data[1];
             String b = data.length == 3 ? data[2] : null;
             return new Command(type, a, b);
+        }
+
+        public Type getType() {
+            return type;
         }
 
         @Override
